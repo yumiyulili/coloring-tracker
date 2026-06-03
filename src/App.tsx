6d74b4dd230f258b799f05b7c4ac3617db5a5ff4
yumiyulili <masha.ava04@gmail.com>
2026-06-03 20:50:19 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import "./App.css";
+
+export default function App() {
+    return (
+        // ГЛАВНЫЙ ФОН ПРИЛОЖЕНИЯ С ГРАДИЕНТОМ
+        <div className="MAIN_BACKGROUND min-h-screen bg-gradient-to-br from-slate-100 via-purple-300 to-slate-100 flex flex-col p-6">
+            {/* Навигационная панель */}
+            <div className="NAVI fixed left-0 top-0 z-50 w-full h-[60px] flex bg-black/20">
+
+            {/* КОНТЕНТНАЯ ЗОНА */}
+
+
+
+
+            </div>
+
+        </div>
+    );
+}
